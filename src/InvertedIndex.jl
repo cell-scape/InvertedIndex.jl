c53596ca94e1bf4ec294540afe281159be854d1d
Bradley Dowling <brad.dowling@protonmail.com>
2022-11-05 06:01:24 -0400

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.29

---
+++ src/InvertedIndex.jl
@@ -0,0 +1,5 @@
+module InvertedIndex
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using InvertedIndex
+using Test
+
+@testset "InvertedIndex.jl" begin
+    # Write your tests here.
+end
